18bb3b48d74ba335d679670a31cef50c299d84c2
Moustaph <103828108+Moustaph0@users.noreply.github.com>
2023-02-14 13:43:45 +0100

    Update countOneToHunds.js

--- countOneToHunds.js
+++ countOneToHunds.js
@@ -1,9 +1,7 @@
 function countOneToHunds(){
-    var i=1;
-    for(i=1;i<100;i++){
+    for(let i=1;i<=100;i++){
        console.log(i);   
     }
-    return i;
 }
-console.log(countOneToHunds(100));
+countOneToHunds();
 module.exports = countOneToHunds;

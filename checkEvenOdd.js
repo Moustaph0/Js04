2def8dd9c0b0b0287c617821ea68d11ab184b49f
Moustaph <103828108+Moustaph0@users.noreply.github.com>
2023-02-14 14:47:09 +0100

    Update checkEvenOdd.js

--- checkEvenOdd.js
+++ checkEvenOdd.js
@@ -1,13 +1,12 @@
 function checkEvenOdd(i){
-    for(i=1;i<=2;i++){
+    
         if(i%2==0){
             console.log("pair");
         }
         else{
             console.log("Impair");
         }
-    }
-    return i;
+
 }
 checkEvenOdd(7);
 checkEvenOdd(2);
